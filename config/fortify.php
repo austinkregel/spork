0314b1c6776b7927e09e94b4d0261bb08719b05f
austinkregel <austinkregel@users.noreply.github.com>
2023-08-06 04:11:31 +0000

    PHP Linting (Pint)

--- config/fortify.php
+++ config/fortify.php
@@ -134,7 +134,7 @@ return [
     */
 
     'features' => [
-//        Features::registration(),
+        //        Features::registration(),
         Features::resetPasswords(),
         Features::emailVerification(),
         Features::updateProfileInformation(),
