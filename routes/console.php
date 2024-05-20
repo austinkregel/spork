adc9d189236852881249d920aec77d364fe3f1b5
Shift <shift@laravelshift.com>
2024-05-20 02:15:57 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,3 +1,16 @@
 <?php
 
+use App\Jobs\FetchResourcesFromCredentials;
+use App\Jobs\News\UpdateAllFeeds;
+use App\Jobs\Notifications\BuildSummaryNotificationJob;
+use App\Jobs\SyncJiraTicketsJob;
+use Illuminate\Support\Facades\Schedule;
+
 declare(strict_types=1);
+
+
+Schedule::job(SyncJiraTicketsJob::class)->daily();
+Schedule::job(UpdateAllFeeds::class)->everyFifteenMinutes();
+Schedule::job(FetchResourcesFromCredentials::class)->hourly();
+Schedule::job(BuildSummaryNotificationJob::class)->dailyAt('13:00');
+Schedule::command('operations:queue')->everyFiveMinutes();
