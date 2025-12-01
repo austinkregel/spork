82604330aea29e75d0758333160f6220c3a6950a
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2025-12-01 17:48:05 -0500

    Reenable Matrix job

--- routes/console.php
+++ routes/console.php
@@ -14,7 +14,7 @@ Schedule::job(BuildSummaryNotificationJob::class)->dailyAt('13:00');
 Schedule::command('operations:queue')->everyMinute();
 Schedule::command('automations:seed-next')->everyMinute();
 // Temporarily disabled due to the server going down
-//Schedule::job(MatrixSyncJob::class)->everyMinute();
+Schedule::job(MatrixSyncJob::class)->everyMinute();
 
 Artisan::command('update:all-feeds', function () {
     UpdateAllFeeds::dispatch();
