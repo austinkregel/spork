1ca95944aa8d3204f1158711d722909a76fa9958
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2025-12-01 17:45:22 -0500

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -6,14 +6,13 @@ use App\Jobs\FetchResourcesFromCredentials;
 use App\Jobs\MatrixSyncJob;
 use App\Jobs\News\UpdateAllFeeds;
 use App\Jobs\Notifications\BuildSummaryNotificationJob;
-use App\Jobs\SyncJiraTicketsJob;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::job(SyncJiraTicketsJob::class)->daily();
 Schedule::job(UpdateAllFeeds::class)->everyFifteenMinutes();
 Schedule::job(FetchResourcesFromCredentials::class)->hourly();
 Schedule::job(BuildSummaryNotificationJob::class)->dailyAt('13:00');
-Schedule::command('operations:queue')->everyFiveMinutes();
+Schedule::command('operations:queue')->everyMinute();
+Schedule::command('automations:seed-next')->everyMinute();
 // Temporarily disabled due to the server going down
 //Schedule::job(MatrixSyncJob::class)->everyMinute();
 
@@ -34,54 +33,4 @@ Artisan::command('sync:matrix', function () {
 })->describe('Sync Matrix');
 Artisan::command('sync:transaction-tags', function () {
     dispatch_sync(new \App\Jobs\SyncTagsWithTransactionsInDatabase);
-});
-
-Artisan::command('test', function () {
-    $template = view('docker.basic-service', [
-        'projects' => [
-            [
-                'path' => 'spork',
-                'queue' => true,
-                'web' => true,
-                'websocket' => true,
-                'cron' => true,
-                'image' => 'ghcr.io/austinkregel/spork:latest',
-                'dependencies' => [
-                ],
-                'command' => 'bash /var/www/artisan'
-            ],
-            [
-                'path' => 'lazy.build',
-                'queue' => false,
-                'web' => true,
-                'websocket' => false,
-                'cron' => false,
-                'volumes' => [],
-                'image' => 'ghcr.io/austinkregel/lazy.build:latest',
-                'dependencies' => [
-                ],
-            ],
-            [
-                'path' => 'cannabis-consumer-information',
-                'queue' => true,
-                'web' => true,
-                'websocket' => false,
-                'cron' => true,
-                'image' => 'ghcr.io/austinkregel/cannabis-consumer-information:latest',
-                'dependencies' => [
-                ],
-            ],
-            [
-                'path' => 'aut.hair',
-                'queue' => false,
-                'web' => true,
-                'websocket' => false,
-                'cron' => false,
-                'image' => 'ghcr.io/austinkregel/aut.hair:latest',
-                'dependencies' => [
-                ],
-            ],
-        ]
-    ]);
-file_put_contents('template.yml', $template->toHtml());
 });
